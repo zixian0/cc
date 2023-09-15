a7b73cd5c68fc29d5799d3a120a28008b6ea0c5a
How Zi Xian <141216021+zixian0@users.noreply.github.com>
2023-09-15 22:21:24 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-customhost = "itp.c4qlbl5ajqxe.us-east-1.rds.amazonaws.com" 
+customhost = "itp.crdowazmqmbg.us-east-1.rds.amazonaws.com" 
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "itp" 
